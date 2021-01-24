495d591f27d6a372a25c05e9f826daf293380795
栗田ちはる <>
2021-01-24 14:12:39 +0900

    index.phpの追加

--- training/index.php
+++ training/index.php
@@ -1,5 +1,17 @@
+<?php
+    echo 'Hello World!';
+?>
+
+<!DOCTYPE html>
 <html>
-<body>
-    hello
-</body>
+    <head>
+        <title>記録帳</title>
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+        <!-- CSS -->
+        <link rel="stylesheet" href="css/index.css?<?php echo date('Ymd-His'); ?>">
+    </head>
+
+    <body>
+    </body>
 </html>
